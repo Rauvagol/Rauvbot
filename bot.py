40eca57d1a99f823f05feaf2bb0239a354f35531
Rauvagol <akirbsy@gmail.com>
2020-05-20 18:59:52 -0700

    Update bot.py
    changed date for test command

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='test', help='reports when this file was updated if I remember')
 async def testcommand(ctx):
-    await ctx.send("May 18th version.")
+    await ctx.send("May 20th version.")
 	
 @bot.command(name='rslookup')
 async def RSlookup(ctx, *name):

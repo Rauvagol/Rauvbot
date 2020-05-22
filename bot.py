f47c0c383a755f1259c351c51c27b6939f6f16c0
Rauvagol <akirbsy@gmail.com>
2020-05-21 20:44:17 -0700

    Update bot.py
    added new runescape api command and one to display a list of commands

--- bot.py
+++ bot.py
@@ -19,9 +19,13 @@ async def testcommand(ctx):
 @bot.command(name='10seconds')
 async def tenSeconds(ctx):
 	await ctx.send("https://i.imgur.com/tnJtepM.jpg")
+
+@bot.command(name='commands')
+async def commands(ctx)
+	await ctx.send("Current commands are !test, !10seconds, !commands, !rslevels, !rskc")
 	
-@bot.command(name='rslookup')
-async def RSlookup(ctx, *name):
+@bot.command(name='rslevels')
+async def rslevels(ctx, *name):
 	#Variables used in the code, each list is for a column of information.
 	username = ' '.join([str(word) for word in name]) 
 	skillName= ["Skill Name", "Total:", "Attack:", "Defence:", "Strength:", "Hitpoints:", "Ranged:", "Prayer:", "Magic:", "Cooking:", "Woodcutting:", "Fletching:", "Fishing:", "Firemaking:", "Crafting:", "Smithing:", "Mining:", "Herblore:", "Agility:", "Thieving:", "Slayer:", "Farming:", "Runecraft:", "Hunter:", "Construction:"]
@@ -29,7 +33,6 @@ async def RSlookup(ctx, *name):
 	skillExperience = ["Experience"]
 	output = ""
 	exptotal = 0
-
 	#Combines and parses the url to access the OSRS highscores api page for given character name
 	await ctx.send("Looking up " + username + ", please be patient, the API is very slow sometimes.")
 	try:
@@ -37,10 +40,10 @@ async def RSlookup(ctx, *name):
 	except:
 		await ctx.send("An error occurred, probably a 404, but what do I know? I just work here. Check the spelling of your username btw.")
 	#Takes the JSON data from the url, decodes it using utf-8, throws away all information after the experience, and splits entries on newlines
-	split_rle = data.read().decode(data.headers.get_content_charset("utf-8")).split("-1,-1")[0].split("\n")
+	dataHolder = data.read().decode().split("\n")
 	#Takes each entry in the previous list, splits into the 3 parts, and assigns each to the appropriate column (discarding rank)
-	for index in range (len(split_rle)-1):
-		holder = split_rle[index].split(",")
+	for index in range (len(skillName)-1):
+		holder = dataHolder[index].split(",")
 		skillLevel.append(holder[1])
 		skillExperience.append(holder[2])
 		if(index>0):
@@ -64,6 +67,38 @@ async def RSlookup(ctx, *name):
 			#loops through adding one formatted row at a time to the output list
 			outputList.append(" ║ " + skillName[index].ljust(len(spacer_one)) + " ║ " + skillLevel[index].rjust(len(spacer_two)) + " ║ " + skillExperience[index].rjust(len(spacer_three)) + " ║\n")
 	await ctx.send("```" + header+"".join(outputList)+footer + "```")
+
+@bot.command(name="rskc")
+async def rskc(ctx, *name):
+	username = ' '.join([str(word) for word in name]) 
+	kcName = ["Name","Unknown1","Unknown2","Unknown3","Clue Scrolls","Beginner Clues","Easy Clues","Medium Clues","Hard Clues","Elite Clues","Master Clues",
+				"Unknown4","Abyssal Sire","Hydra","Barrows","Unknown7","Unknown8","Cerberus","Unknown9","Unknown10","Unknown11","Unknown12","Unknown13",
+				"Unknown14","Unknown15","Unknown16","Dagganoth Rex","Unknown17","Unknown18","Unknown19","Unknown20","Unknown21","Hespori","Kalphite Queen",
+				"Unknown23","Kraken","Unknown24","K'ril","Unknown25","Unknown26","Unknown27","Unknown28","Unknown29","Skotizo","Unknown30","Unknown31",
+				"Unknown32","Thermonuclear","Unknown33","Unknown34","Unknown35","Unknown36","Vorkath","Wintertodt","Zalcano","Zulrah"]
+	kcCount = ["Kills"]
+	numSkills = 23
+	output = ""
+	outputList = []
+	await ctx.send("Looking up " + username + ", please be patient, the API is very slow sometimes.")
+	try:
+		data=urllib.request.urlopen("https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player="+username.replace(" ", "%20"))
+	except:
+		await ctx.send("An error occurred, probably a 404, but what do I know? I just work here. Check the spelling of your username btw.")
+	#Takes the JSON data from the url, decodes it using utf-8, throws away all information after the experience, and splits entries on newlines
+	dataHolder = data.read().decode().split("\n")
+	for index in range (numSkills+1, len(dataHolder)-1):
+		holder = dataHolder[index].split(",")
+		kcCount.append(holder[1])
+	spacer_one = "═".ljust(len(max(kcName, key = len)), "═")
+	spacer_two = "═".ljust(len(max(kcCount, key = len)), "═")
+	tableWidth=9 + len(spacer_one+spacer_two)
+	header = " ╔═"+spacer_one+"═══"+spacer_two+"═╗\n" + " ║ "+"KC for " + username + "║".rjust(tableWidth-len(" ║ "+"KC for " + username)-1)+"\n" +  " ╠═"+spacer_one+"═╦═"+spacer_two+"═╣\n" +  " ║ " + kcName[0].ljust(len(spacer_one)) + " ║ " + kcCount[0].rjust(len(spacer_two)) + " ║\n" + " ╠═"+spacer_one+"═╬═"+spacer_two+"═╣\n"
+	footer = " ╚═"+spacer_one+"═╩═"+spacer_two+"═╝\n"
+	for index in range (len(kcName)):
+		if(index>0 and int(kcCount[index])>0):
+			outputList.append(" ║ " + kcName[index].ljust(len(spacer_one)) + " ║ " + kcCount[index].rjust(len(spacer_two)) + " ║\n")
+	await ctx.send("```" + header + "".join(outputList) + footer + "```")
 
 @bot.command(name='maze')
 async def MazeGenerator(ctx, width, height):

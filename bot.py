8b5e474954989447c61964632559dce81ac73311
Adam <akirbsy@gmail.com>
2024-08-08 18:32:40 -0700

    change test number

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ async def on_ready():
 @bot.command(name='test', help='for testing')
 async def test_command(ctx):
     print("running")
-    await ctx.send("Running2")
+    await ctx.send("Running3")
 
 
 @bot.event
